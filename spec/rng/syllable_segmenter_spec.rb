451c8eb4f26d78910389be6744551610f98a9832
Justin Gaylor <justin.gaylor@gmail.com>
2012-05-29 17:16:01 +0900

    Updated spec for syllable segmenter

--- spec/rng/syllable_segmenter_spec.rb
+++ spec/rng/syllable_segmenter_spec.rb
@@ -2,43 +2,39 @@ require 'rng/syllable_segmenter'
 
 
 describe Rng::SyllableSegmenter do
-  it "can segment single syllable names" do
-    syllables = Rng::SyllableSegmenter.segment("Thor")
+  it 'can segment single syllable names' do
+    syllables = Rng::SyllableSegmenter.segment('Thor')
     syllables.should_not be_nil
     syllables.should be_an(Array)
     syllables.size.should == 1
-    syllables[0].to_s.should == "thor"
   end
   
-  it "can segment double syllable names" do
-    syllables = Rng::SyllableSegmenter.segment("Thorin")
+  it 'segments double syllable names' do
+    syllables = Rng::SyllableSegmenter.segment('Thorin')
     syllables.should_not be_nil
     syllables.should be_an(Array)
     syllables.size.should == 2
-    syllables[0].to_s.should == "tho"
-    syllables[1].to_s.should == "rin"
   end
   
-  it "can segment triple syllable names" do
-    syllables = Rng::SyllableSegmenter.segment("Mephisto")
+  it 'segments triple syllable names' do
+    syllables = Rng::SyllableSegmenter.segment('Mephisto')
     syllables.should_not be_nil
     syllables.should be_an(Array)
     syllables.size.should == 3
-    syllables[0].to_s.should == "meph"
-    syllables[1].to_s.should == "is"
-    syllables[2].to_s.should == "to"
   end
   
-  it "can segment loooooooooooong names" do
-    syllables = Rng::SyllableSegmenter.segment("Mephistopheles")
+  it 'segments loooooooooooong names' do
+    syllables = Rng::SyllableSegmenter.segment('Mephistopheles')
     syllables.should_not be_nil
     syllables.should be_an(Array)
     syllables.size.should == 5
-    syllables[0].to_s.should == "meph"
-    syllables[1].to_s.should == "ist"
-    syllables[2].to_s.should == "o"
-    syllables[3].to_s.should == "phe"
-    syllables[4].to_s.should == "les"
   end
   
+  it 'segments names into non-overlapping, non-empty syllables' do
+    name = 'Andromeda'
+    syllables = Rng::SyllableSegmenter.segment(name)
+    syllables.should_not be_nil
+    syllables.should be_an(Array)
+    syllables.join('').should == name.downcase
+  end
 end
